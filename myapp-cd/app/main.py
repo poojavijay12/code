18053db29236ddcefc26e0d3a30bb376709e3c33
poojavijay12 <lilgurl3134@gmail.com>
2025-11-06 11:51:59 +0530

    finall

--- myapp-cd/app/main.py
+++ myapp-cd/app/main.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
+# app/main.py
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-@app.route("/")
-def hello():
-    return jsonify({"message": "hello from myapp", "env": "REPLACE_ENV"})
+app = FastAPI()
 
+@app.get("/")
+async def hello():
+    return JSONResponse(content={"message": "hello from myapp", "env": "REPLACE_ENV"})
+
+# For local run (optional)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
